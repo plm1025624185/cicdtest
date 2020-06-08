a3fba30a6bc1de82df768da16ba7a7b27b5531e2
unknown <panleiming@worktrans.cn>
2020-06-08 21:02:42 +0800

    ci

--- src/main/java/com/example/cicdtest/controller/TestController.java
+++ src/main/java/com/example/cicdtest/controller/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
-    @GetMapping
+    @GetMapping("/test")
     public String test() {
         return "test111";
     }
